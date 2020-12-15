7bd17b3fd30f6433dbaa117f11cace5979488374
suban-khoja-777 <69012061+suban-khoja-777@users.noreply.github.com>
2020-12-15 11:01:20 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 function app_router(){
     firebase.auth().onAuthStateChanged((user) => {
         if (user) {
-            window.open('//pages/app.html',"_self");
+            window.open('./pages/app.html',"_self");
         } else {
-            window.open('//pages/auth.html',"_self");
+            window.open('./pages/auth.html',"_self");
         }
       });
 }
