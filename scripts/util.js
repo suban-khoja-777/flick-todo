28b74e36902cad8b8315614db31af66f37b74635
suban-khoja-777 <69012061+suban-khoja-777@users.noreply.github.com>
2020-12-18 20:36:29 +0530

    Delete util.js

--- scripts/util.js
+++
@@ -1,7 +0,0 @@
-const showSpinner = () =>  {
-    document.querySelector('.app-spinner').classList.remove('hide');
-}
-
-const hideSpinner = () =>  {
-    document.querySelector('.app-spinner').classList.add('hide');
-}
