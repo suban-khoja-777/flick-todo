5ada5b7f6d887a8feb856792ea56ac9601f10a81
suban-khoja-777 <69012061+suban-khoja-777@users.noreply.github.com>
2020-12-18 20:36:27 +0530

    Delete main.js

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-function app_router(){
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            window.open('../flick-todo/pages/app.html',"_self");
-        } else {
-            window.open('../flick-todo/pages/auth.html',"_self");
-        }
-      });
-}
